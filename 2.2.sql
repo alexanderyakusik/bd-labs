e3504c866496a99c8ab9e29b406f435bfe357a02
Alexander Yakusik <alexander.yakusik@gmail.com>
2018-09-22 20:09:04 +0300

    Finished 2.2 task

--- 2.2.sql
+++ 2.2.sql
@@ -2,13 +2,13 @@ USE AdventureWorks2012;
 GO
 
 CREATE TABLE dbo.StateProvince (
-	StateProvinceID		    INT			 NOT NULL,
-	StateProvinceCode       NCHAR(3)	 NOT NULL,
-	CountryRegionCode       NVARCHAR(3)  NOT NULL,
-	IsOnlyStateProvinceFlag BIT			 NOT NULL,
-	Name					NVARCHAR(50) NOT NULL,
-	TerritoryID				INT			 NOT NULL,
-	ModifiedDate			DATETIME	 NOT NULL
+    StateProvinceID         INT          NOT NULL,
+    StateProvinceCode       NCHAR(3)     NOT NULL,
+    CountryRegionCode       NVARCHAR(3)  NOT NULL,
+    IsOnlyStateProvinceFlag BIT          NOT NULL,
+    Name                    NVARCHAR(50) NOT NULL,
+    TerritoryID             INT          NOT NULL,
+    ModifiedDate            DATETIME     NOT NULL
 );
 GO
 
@@ -21,3 +21,28 @@ ADD CHECK (TerritoryID % 2 = 0);
 ALTER TABLE dbo.StateProvince
 ADD DEFAULT 2 FOR TerritoryID;
 GO
+
+INSERT INTO dbo.StateProvince (
+    StateProvinceID,
+    StateProvinceCode,
+    CountryRegionCode,
+    IsOnlyStateProvinceFlag,
+    Name,
+    ModifiedDate)
+SELECT TOP 1 WITH TIES
+    stpr.StateProvinceID,
+    stpr.StateProvinceCode,
+    stpr.CountryRegionCode,
+    stpr.IsOnlyStateProvinceFlag,
+    stpr.Name,
+    stpr.ModifiedDate
+FROM Person.StateProvince stpr
+INNER JOIN Person.Address addr                 ON addr.StateProvinceID = stpr.StateProvinceID
+INNER JOIN Person.BusinessEntityAddress beaddr ON beaddr.AddressID = addr.AddressID
+INNER JOIN Person.AddressType addrtp           ON addrtp.AddressTypeID = beaddr.AddressTypeID
+WHERE addrtp.Name = 'Shipping'
+ORDER BY ROW_NUMBER() OVER (PARTITION BY stpr.StateProvinceID, stpr.StateProvinceCode
+                            ORDER BY addr.AddressID desc);
+
+ALTER TABLE dbo.StateProvince
+ALTER COLUMN IsOnlyStateProvinceFlag SMALLINT NULL;
