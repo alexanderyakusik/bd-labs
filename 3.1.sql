7b2a3f8deb424f9290823c220a78e6e4e98b2514
Alexander Yakusik <alexander.yakusik@gmail.com>
2018-09-23 17:09:12 +0300

    Completed 3.1

--- 3.1.sql
+++ 3.1.sql
@@ -43,3 +43,46 @@ FROM
     dbo.StateProvince dest
     INNER JOIN @StateProvince src      ON src.StateProvinceID = dest.StateProvinceID
     INNER JOIN Person.CountryRegion cr ON cr.CountryRegionCode = src.CountryRegionCode;
+
+DELETE dbo.StateProvince
+WHERE StateProvinceID NOT IN (
+    SELECT TOP 1 WITH TIES
+        StateProvinceID
+    FROM dbo.StateProvince
+    ORDER BY ROW_NUMBER() OVER (PARTITION BY AddressType
+                                ORDER BY StateProvinceID DESC)
+);
+
+DECLARE @ConstraintName NVARCHAR(50);
+
+DECLARE ConstraintsCursor CURSOR FOR
+SELECT
+    CONSTRAINT_NAME
+FROM AdventureWorks2012.INFORMATION_SCHEMA.CONSTRAINT_TABLE_USAGE
+WHERE TABLE_SCHEMA = 'dbo'
+  AND TABLE_NAME = 'StateProvince'
+UNION
+SELECT
+    dc.name
+FROM sys.default_constraints dc
+INNER JOIN sys.columns c ON c.column_id = dc.parent_column_id
+WHERE dc.parent_object_id = OBJECT_ID(N'dbo.StateProvince');
+
+OPEN ConstraintsCursor;
+
+FETCH NEXT FROM ConstraintsCursor
+INTO @ConstraintName;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    EXEC('ALTER TABLE dbo.StateProvince
+          DROP CONSTRAINT ' + @ConstraintName);
+
+    FETCH NEXT FROM ConstraintsCursor
+    INTO @ConstraintName;
+END;
+
+CLOSE ConstraintsCursor;
+DEALLOCATE ConstraintsCursor;
+
+DROP TABLE dbo.StateProvince;
